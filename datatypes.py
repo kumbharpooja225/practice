5733d99c021b63fb6e04837a2ae0c2cb71715ce2
POOJA KUMBHAR <165540601+kumbharpooja225@users.noreply.github.com>
2025-01-18 20:02:20 +0530

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#Write a program in Jupyter Notebook to declare variables of different data types (integer, float, string, and boolean). 
+#Print each variable and its type.
+a=355
+b=8.38
+c="pooja"
+d=True
+
+
+# In[3]:
+
+
+a
+
+
+# In[4]:
+
+
+b
+
+
+# In[5]:
+
+
+c
+
+
+# In[6]:
+
+
+d
+
+
+# In[7]:
+
+
+type(a)
+
+
+# In[8]:
+
+
+type(b)
+
+
+# In[9]:
+
+
+type(c)
+
+
+# In[10]:
+
+
+type(d)
+
+
+# In[ ]:
+
+
+
+
